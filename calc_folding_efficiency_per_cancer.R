5d52c920bfa180f906d7ee25997ed2134824e981
Geutg <geut.galai@gmail.com>
2021-07-13 23:04:23 +0300

    adding a script to calculated folding percentage per cancer. done by dividing the chap degree by folding potential (the number of protein a chaperone can fold across all cancers).

---
+++ calc_folding_efficiency_per_cancer.R
@@ -0,0 +1,87 @@
+#------------------------------
+# build a table that shows the percent of the folding potential that is
+# activated for each chaperonr for every cancer.
+# 
+#------------------------------
+
+
+#------ includes ------
+library(readxl)
+library(ggplot2)
+library(pheatmap)
+
+#-------------- load the networks from an excel file --------
+excel_path <- "HPC/binari_validated_corrs.xlsx"
+
+# do the convert for every cancer
+sheet_names <- excel_sheets(excel_path)
+
+networks <- list()
+for (name in sheet_names) {
+  x <- as.data.frame(read_excel(excel_path, sheet = name, col_names = TRUE))
+  x2 <- x[,-1]
+  rownames(x2) <- x[,1]
+  networks[[name]] <- x2
+}
+
+#----------- create union of the cancer networks -------------
+union_table <- networks[[1]]
+union_table[,] <- 0 
+
+for (i in 1:length(networks)) {
+  net <- networks[[i]]
+  union_table <- union_table + net
+}
+union_table_mat <- as.matrix(union_table)
+
+#----------- flat the union and calc folding potential -------
+binari_union <- 1*(union_table>0)
+
+chap_potential <- rowSums(binari_union, na.rm = FALSE, dims = 1)
+chap_potential
+
+#----------- calc chaperon degree per cancer -----------------
+# get the list of proteins ENSID as rownames, SYBOL as first row
+chaps_meta <- read.table("HPC/Mito_ch_genes.tab", sep="\t", header=TRUE, 
+                         stringsAsFactors=FALSE, quote="", fill=FALSE)
+Symbol <- chaps_meta[, 4]
+degree_table_chap <- as.data.frame(Symbol)
+
+# go over the cancers
+for (i in 1:length(networks)) {
+  netname <- names(networks)[i]
+  net <- networks[[i]]
+  
+  # go over the proteins
+  chap_degrees <- c()
+  for (chap in 1:nrow(chaps_meta)) {
+    SYMB <- chaps_meta[chap, "Symbol"]
+    # calc relative degree
+    
+    chap_degrees[SYMB] <- sum(net[SYMB, ])
+  }
+  
+  # put each cancer in a new column
+  curr_cancer_degrees <- data.frame(chap_degrees)
+  new_col_names <- c(colnames(degree_table_chap), netname)
+  
+  degree_table_chap <- merge(degree_table_chap, curr_cancer_degrees,
+                             by.x="Symbol", by.y="row.names", all.x=TRUE)
+  degree_table_chap
+  
+  colnames(degree_table_chap) <- new_col_names
+}
+
+
+#----------- calculate percent of potential used ----------
+row.names(degree_table_chap) <- degree_table_chap$Symbol
+folding_percent <- degree_table_chap[,-1]
+
+for (chapp in chaps_meta$Symbol) {
+  chap_pot <- chap_potential[chapp]
+  folding_percent[chapp, ] <- folding_percent[chapp,]/chap_pot
+}
+
+write.csv(folding_percent*100, file = "output/chap_folding_percent.csv")
+
+pheatmap(folding_percent)
